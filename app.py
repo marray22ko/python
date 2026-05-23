9ac81f33e8dd7819048286d450bdaa4a0d65aec1
marray22ko <marray22@gmail.com>
2026-05-23 14:51:42 +0900

    push

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ st.write("여기서 원하는 앱을 선택하세요.")
 if st.button("조건 필터형 퀀트 검색기 열기"):
     st.markdown('<a href="https://python-7qve9iqwvzj8n5kzezmvoq.streamlit.app/" target="_blank">👉 이동하기</a>', unsafe_allow_html=True)
 
-if st.button("가치투자 분석 열기"):
+if st.button("가치투자 분석 열기(나스닥)"):
+    st.markdown('<a href="https://python-gmzcymqjhkdah3aamk2uaa.streamlit.app/" target="_blank">👉 이동하기</a>', unsafe_allow_html=True)
+
+if st.button("가치투자 분석 열기(코스닥)"):
     st.markdown('<a href="https://python-gmzcymqjhkdah3aamk2uaa.streamlit.app/" target="_blank">👉 이동하기</a>', unsafe_allow_html=True)
